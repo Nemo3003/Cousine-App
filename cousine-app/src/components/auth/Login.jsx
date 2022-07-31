35ed346ba0fcf0d0400359f3e17cdef766ac7c65
Nemo3003 <ulimarian2000@gmail.com>
2022-07-30 22:07:24 -0300

    something done

--- cousine-app/src/components/auth/Login.jsx
+++ cousine-app/src/components/auth/Login.jsx
@@ -1,6 +1,9 @@
 export const Login = () => {
+
+    const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_url=http://127.0.0.1:5173/music&scope=%20';
     return(
         <>
+        
         </>
     )
 }
